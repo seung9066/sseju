630c1b20290e7e79f7cfe576adc20f131084dc9e
seunghyun choi <99865533+seung9066@users.noreply.github.com>
2022-11-15 20:59:11 +0900

    오류해결,회원가입 암호

--- sseju/src/main/java/com/sseju/java/company/web/CompanyController.java
+++ sseju/src/main/java/com/sseju/java/company/web/CompanyController.java
@@ -1,6 +1,7 @@
 package com.sseju.java.company.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,6 +16,10 @@ public class CompanyController {
 	
 	@PostMapping("/newCpAccount")
 	public String cpAccount(CompanyVO vo) {
+		BCryptPasswordEncoder enco = new BCryptPasswordEncoder();
+		String pw = vo.getPassword();
+		vo.setPassword(enco.encode(pw));
+		
 		service.insertCompany(vo);
 		service.insertLogin(vo);
 		return "login";
