2156bf488c34a39e7018d52f6018f0873c68223d
User <User@DESKTOP-Lenovo>
2022-11-18 02:24:39 +0900

    1118 김민지1

--- sseju/src/main/java/com/sseju/java/eqm/service/EqmVO.java
+++ sseju/src/main/java/com/sseju/java/eqm/service/EqmVO.java
@@ -10,21 +10,37 @@ import lombok.Data;
 @Data
 @Alias("EqmVO")
 public class EqmVO {
-	private String eqmCode;
-	private String eqmName;
-	private String eqmModel;
-	private String eqmSize;
-	private String eqmLoc;
-	private int eqmMaxTemp;
-	private int eqmMinTemp;
-	private String eqmCheck;
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private Date eqmPurcDate;
-	private String eqmDiv;
-	private String eqmImg;
-	private String eqmPurcName;
-	private String eqmPurcTel;
-	private String eqmYn;
-	private String lineNo;
+   //설비
+   private String eqmCode;
+   private String eqmName;
+   private String eqmModel;
+   private String eqmSize;
+   private String eqmLoc;
+   private int eqmMaxTemp;
+   private int eqmMinTemp;
+   private String eqmCheck;
+   @DateTimeFormat(pattern = "yyyy-MM-dd")
+   private Date eqmPurcDate;
+   private String eqmDiv;
+   private String eqmImg;
+   private String eqmPurcName;
+   private String eqmPurcTel;
+   private String eqmYn;
+   
+   // 라인
+   private String lineNo; // 라인 번호
+   private int totPrc; // 총생산량
+   private int uph; //UPH
+   private String empNo; //담당자
+   private String lineYn; // 사용여부
+   
+   //정기점검
+   private String chkNo; //정기점검 이력번호
+   @DateTimeFormat(pattern = "yyyy-MM-dd")
+   private Date chkDate; // 점검일자
+   private Date chkNdate; // 차기 점검일자
+   private String chkState; //판정
+   private String chkSol; //조치사항
+   private String chkEmpNo; //점검담당자
 
 }
--- sseju/src/main/java/com/sseju/java/eqm/web/EqmController.java
+++ sseju/src/main/java/com/sseju/java/eqm/web/EqmController.java
@@ -1,37 +1,51 @@
 package com.sseju.java.eqm.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sseju.java.eqm.service.EqmService;
+import com.sseju.java.eqm.service.EqmVO;
 
 @Controller
 public class EqmController {
-	
-	@Autowired
-	   EqmService eqmService;
+   
+   @Autowired
+      EqmService eqmService;
 
-	
-	  @RequestMapping("/eqmList")
-	   public String getEqmList(Model model) { //데이터를 보낼때 model
-	      model.addAttribute("eqmList", eqmService.selectEqmList());
-	      return "/admin/eqm/eqmList";
-	   }
-	  
-	  @GetMapping("/eqmCheck")
-		public String eqmCheck() {
-			return "/admin/eqm/eqmCheck";
-		}
-	  @GetMapping("/eqmUoper")
-		public String eqmUoper() {
-			return "/admin/eqm/eqmUoper";
-		} 
-	  
+   
+     @GetMapping("/eqmList")
+      public String eqmList(Model model) {
+        
+         return "/admin/eqm/eqmList";
+      }
+     
+     @GetMapping("/eqmCheck")
+      public String eqmCheck() {
+         return "/admin/eqm/eqmCheck";
+      }
+     @GetMapping("/eqmUoper")
+      public String eqmUoper() {
+         return "/admin/eqm/eqmUoper";
+      } 
+     
+     @GetMapping("/lineList")
+     @ResponseBody
+     public List<EqmVO> getEqmLineList(Model model){
+        return eqmService.getEqmLineList();
+     }
+     
+  
+     
+     
 
-	
+   
 
 
 }
