7ef59a6f3c466b0c4ae5011e0ebdc2aceefc17a9
kjh2022 <fhqlxm123@daum.net>
2022-11-21 07:24:38 +0900

    html수정

---
+++ sseju/src/main/java/com/sseju/java/matrecchk/service/impl/MatRecChkServiceImpl.java
@@ -0,0 +1,43 @@
+package com.sseju.java.matrecchk.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sseju.java.matrecchk.service.MatRecChkService;
+import com.sseju.java.matrecchk.service.MatRecChkVO;
+
+@Service
+public class MatRecChkServiceImpl implements MatRecChkService {
+
+	@Override
+	public int insertMatRecChk(MatRecChkVO mrcVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<MatRecChkVO> getMatRecChkList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MatRecChkVO getMatRecChkInfo(MatRecChkVO mrcVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateMatRecChk(MatRecChkVO mrcVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteMatRecChk(MatRecChkVO mrcVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
