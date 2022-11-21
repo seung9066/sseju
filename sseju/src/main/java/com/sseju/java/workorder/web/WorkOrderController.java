0d6bae8cd57ac06d289077d2343f17154e2afb3f
junhyoung <kjhyoung4196@gmail.com>
2022-11-21 17:51:11 +0900

    1121_2whsskdjfudnjtjahtgoajrrptspTlQkf CRUD진행중이라는 뜻

--- sseju/src/main/java/com/sseju/java/workorder/web/WorkOrderController.java
+++ sseju/src/main/java/com/sseju/java/workorder/web/WorkOrderController.java
@@ -1,11 +1,13 @@
 package com.sseju.java.workorder.web;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sseju.java.workorder.serivce.WorkOrderService;
@@ -18,13 +20,20 @@ public class WorkOrderController {
 	@Autowired
 	WorkOrderService woService;
 	
-	//작업 지시 전체 조회 -페이지 연결만 해둠 기능X
+	//작업 지시 페이지 띄움
 	@GetMapping("/workOrder")
 	public String WorkOrderList() {
-		//model.addAttribute("workOrderList", woService.getWorkOrderList());
 		return "/admin/produce/workOrderList";
 	}
 	
+    //지시 목록 가져옴
+	@GetMapping("/getWorkOrderList")
+	@ResponseBody
+	public List<WorkOrderVO> getWorkOrderList(){
+        return woService.getWorkOrderList();
+     }
+	   
+	//지시 등록
 	@PostMapping("/insertWorkOrder")
 	@ResponseBody
 	public String insertWorkOrder(WorkOrderVO woVO) {
@@ -32,15 +41,25 @@ public class WorkOrderController {
 		return "redirect:workOrderList";
 	}
 	
-	@PostMapping("/updateWorkOrder")
-	public String updateWorkOrder(WorkOrderVO woVO, Model model) {
-		model.addAttribute("workOrderList", woService.getWorkOrderList());
-		return "/admin/produce/workOrderList";
+	@PostMapping("/deleteWorkOrder")
+	@ResponseBody
+	public int deleteWorkOrder(@RequestParam(value="deleteWorkOrder[]", required=false) List<String> deleteWorkOrder) {
+		int res = 0;
+		for(int i=0; i<deleteWorkOrder.size(); i++) {
+			String line = deleteWorkOrder.get(i);
+			
+			WorkOrderVO woVO = new WorkOrderVO();
+			woVO.setPreNo(line);
+			
+			res += woService.deleteWorkOrder(woVO);
+		}
+		return res;
 	}
 	
-	@PostMapping("/selectDeleteWO")
-	public String deleteWorkOrder(WorkOrderVO woVO, Model model) {
-		model.addAttribute("workOrderList", woService.deleteWorkOrder(woVO));
-		return "/admin/produce/workOrderList";
-	}
+	
+	/*
+	 * @PostMapping("/selectDeleteWO") public String deleteWorkOrder(WorkOrderVO
+	 * woVO, Model model) { model.addAttribute("workOrderList",
+	 * woService.deleteWorkOrder(woVO)); return "/admin/produce/workOrderList"; }
+	 */
 }
